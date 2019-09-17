abbbccee05e4ebdc5bd6ce3d7ff9b0b4920404ba
leucome <leucome@hotmail.com>
2019-09-17 04:15:06 -0400

    Fix for leaf decay and initial third party mod support.

--- init.lua
+++ init.lua
@@ -11,6 +11,8 @@
 
 ]]--
 
+
+
 local wp = minetest.get_worldpath() .. "/luscious"
 minetest.mkdir(wp)
 
@@ -46,9 +48,14 @@ local function on_construct(pos)
 	local map = minetest.decompress(z)
 
 	local node = minetest.get_node(pos)
-	node.param2 = cmpy(string.byte(map, l + 1), pos.y)
+	
+    if node.param2 ~=1 then     -- Use/preseve 1 as the flag to prevent leaf decay.     
+        node.param2 = cmpy(string.byte(map, l + 1), pos.y)
+    end
 	minetest.swap_node(pos, node)
 end
+
+
 
 minetest.override_item("default:dirt_with_grass", {
 	paramtype2 = "color",
@@ -59,8 +66,81 @@ minetest.override_item("default:dirt_with_grass", {
 	tiles = {"luscious_grass.png", {name = "default_dirt.png", color = "white"}, "luscious_grass.png"},
 	overlay_tiles = {"", "", {name = "luscious_dirt_overlay.png", color = "white"}},
 	place_param2 = 136,
+	drop  = "default:dirt",
 	on_construct = on_construct,
 })
+
+
+
+
+minetest.override_item("default:desert_sand", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#c0825dff",
+	palette = "luscious_desert_sand_palette.png",
+	tiles = {"luscious_desert_sand.png", {name = "luscious_desert_sand", color = "white"}, "luscious_desert_sand.png"},
+	place_param2 = 136,
+	drop  = "default:desert_sand", -- Prevent the creation of a new item for each color.
+	on_construct = on_construct,
+})
+
+
+minetest.override_item("default:sand", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#f3dec3ff",
+	palette = "luscious_default_sand_palette.png",
+	tiles = {"luscious_default_sand.png", {name = "default_sand.png", color = "white"}, "luscious_default_sand.png"},
+--	overlay_tiles = {"", "", {name = "luscious_dirt_overlay.png", color = "white"}},
+	place_param2 = 136,
+	drop  = "default:sand",
+	on_construct = on_construct,
+})
+
+
+
+
+
+--------------------------------------Ethereal Add On------------------------------------------------
+if minetest.get_modpath("ethereal") then
+minetest.override_item("ethereal:prairie_dirt", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#7dea1eff",
+	palette = "luscious_grass_palette.png",
+	tiles = {"luscious_prairie_grass.png", {name = "default_dirt.png", color = "white"}, "luscious_prairie_grass.png"},
+	overlay_tiles = {"", "", {name = "luscious_dirt_overlay.png", color = "white"}},
+	place_param2 = 136,
+	drop  = "ethereal:prairie_dirt",
+	on_construct = on_construct,
+})
+
+
+
+
+
+
+minetest.override_item("ethereal:dry_dirt", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#7dea1eff",
+	palette = "luscious_ethereal_dry_dirt_palette.png",
+	tiles = {"luscious_ethereal_dry_dirt.png", {name = "luscious_ethereal_dry_dirt.png", color = "white"}, "luscious_ethereal_dry_dirt.png"},
+--	overlay_tiles = {"", "", {name = "luscious_dirt_overlay.png", color = "white"}},
+	place_param2 = 136,
+	drop  = "ethereal:dry_dirt",
+	on_construct = on_construct,
+})
+
+
+
+end
+
+-----------------------------------------------------------------------------------------------------------
 
 for _, v in pairs({
 	"default:leaves",
@@ -80,6 +160,7 @@ for _, v in pairs({
 		color = "#8afb94ff",
 		tiles = {tile},
 		place_param2 = 136,
+		groups = {snappy = 3, flammable = 2, leaves = 1, leafdecay = 1},
 		on_construct = on_construct,
 	})
 end
@@ -87,6 +168,7 @@ end
 minetest.register_alias_force("default:dirt_with_dry_grass", "default:dirt_with_grass")
 minetest.register_alias_force("default:dirt_with_rainforest_litter", "default:dirt_with_grass")
 minetest.register_alias_force("default:dirt_with_snow", "default:dirt_with_grass")
+
 
 for _, v in pairs({
 	"default:grass_1",
@@ -108,12 +190,18 @@ for _, v in pairs({
 		on_construct = on_construct,
 	})
 end
+
+--- I have absolutly no idea what is the purpose but I aplied it to other stuff and it work.
 for i = 1, 5 do
-	minetest.register_alias_force("default:dry_grass_" .. i, "default:grass_" .. i)
+	minetest.register_alias_force("default:dry_grass_" .. i, "default:grass_" .. i , "default:desert_" .. i , "ethereal:dry_" .. i, "ethereal:prairie_" .. i )
 end
 
 -- content ids
 local cn = {
+	["ethereal:dry_dirt"] = 1,
+	["ethereal:prairie_dirt"] = 1,
+    ["default:sand"] = 1,
+	["default:desert_sand"] = 1,
 	["default:dirt_with_grass"] = 1,
 	["default:grass_1"] = 1,
 	["default:grass_2"] = 1,
