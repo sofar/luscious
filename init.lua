fedfbc483f8c3f4fa78e5448d59351614cff6f38
leucome <leucome@hotmail.com>
2019-09-19 08:56:04 -0400

    Biome info from the api.
    Heat and Humidity is provided by the new api.
    Grass plant are made bigger.
    Added support for  ethereal bamboo dirt

--- init.lua
+++ init.lua
@@ -12,46 +12,26 @@
 ]]--
 
 
-
-local wp = minetest.get_worldpath() .. "/luscious"
-minetest.mkdir(wp)
-
 local mgp = minetest.get_mapgen_params()
 local chunksize = 16 * mgp.chunksize
 
-local function cmpy(p2, y)
-	if y > 0 then
-		local h1 = p2 % 16
-		local h2 = math.floor(p2 / 16) * 16
-
-		h1 = h1 - math.max(math.min(h1, math.floor(y / 16)), 0)
-		return math.max(1, h1 + h2)
-	else
-		return math.max(1, p2)
-	end
+local function chose_color(pos)
+    -- Calculate color from biome heat and humidity.
+    local biome_data = minetest.get_biome_data(pos)
+    local hum1 = biome_data.humidity
+    local temp1 = biome_data.heat
+    local vh = math.floor(math.min(math.max(math.floor(hum1), 0), 100) / 6.6)
+	local vt = math.floor(math.min(math.max(math.floor(temp1), 0), 100) / 6.6)
+	local c1 = (vt + (vh * 16))
+    --minetest.chat_send_all(temp1)
+    return c1
 end
+
+
 local function on_construct(pos)
-	-- get chunk from pos
-	local v = vector.apply(pos, function(a) return math.floor((a - 48) / chunksize) end)
-	local o = vector.subtract(pos, vector.apply(v, function(a) return (a * chunksize) + 48 end))
-	local l = o.z * (chunksize) + o.x
-	local p = minetest.hash_node_position(v)
-
-	local f = io.open(wp .. "/" .. string.format("%d", p), "r")
-	if not f then
-		minetest.log("error", "unable to find map for " .. string.format("%d", p))
-		return
-	end
-
-	local z = f:read("*a")
-	f:close()
-	local map = minetest.decompress(z)
-
-	local node = minetest.get_node(pos)
-	
-    if node.param2 ~=1 then     -- Use/preseve 1 as the flag to prevent leaf decay.     
-        node.param2 = cmpy(string.byte(map, l + 1), pos.y)
-    end
+	-- get chunk from pos and apply color
+    local node = minetest.get_node(pos)
+    node.param2 = chose_color(pos)
 	minetest.swap_node(pos, node)
 end
 
@@ -79,7 +59,7 @@ minetest.override_item("default:desert_sand", {
 	palette_index = 136,
 	color = "#c0825dff",
 	palette = "luscious_desert_sand_palette.png",
-	tiles = {"luscious_desert_sand.png", {name = "luscious_desert_sand", color = "white"}, "luscious_desert_sand.png"},
+	tiles = {"luscious_desert_sand.png", {"luscious_desert_sand.png"}, "luscious_desert_sand.png"},
 	place_param2 = 136,
 	drop  = "default:desert_sand", -- Prevent the creation of a new item for each color.
 	on_construct = on_construct,
@@ -120,6 +100,18 @@ minetest.override_item("ethereal:prairie_dirt", {
 
 
 
+minetest.override_item("ethereal:bamboo_dirt", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#7dea1eff",
+	palette = "luscious_grass_palette.png",
+	tiles = {"luscious_ethereal_grass_bamboo_top.png", {name = "default_dirt.png", color = "white"}, "luscious_ethereal_grass_bamboo_top.png"},
+	overlay_tiles = {"", "", {name = "luscious_dirt_overlay.png", color = "white"}},
+	place_param2 = 136,
+	drop  = "ethereal:bamboo_dirt",
+	on_construct = on_construct,
+})
 
 
 
@@ -130,12 +122,10 @@ minetest.override_item("ethereal:dry_dirt", {
 	color = "#7dea1eff",
 	palette = "luscious_ethereal_dry_dirt_palette.png",
 	tiles = {"luscious_ethereal_dry_dirt.png", {name = "luscious_ethereal_dry_dirt.png", color = "white"}, "luscious_ethereal_dry_dirt.png"},
---	overlay_tiles = {"", "", {name = "luscious_dirt_overlay.png", color = "white"}},
 	place_param2 = 136,
 	drop  = "ethereal:dry_dirt",
 	on_construct = on_construct,
 })
-
 
 
 end
@@ -177,7 +167,13 @@ for _, v in pairs({
 	"default:grass_4",
 	"default:grass_5",
 	"default:junglegrass",
-}) do
+}) do            local p = vector.new(x, y, z)
+            local biome_data = minetest.get_biome_data(p)
+            local hum1 = biome_data.humidity
+            local temp1 = biome_data.heat
+            local vh = math.floor(math.min(math.max(math.floor(hum1), 0), 100) / 6.6)
+            local vt = math.floor(math.min(math.max(math.floor(temp1), 0), 100) / 6.6)
+            local v1 = (vt + (vh * 16))
 	local def = minetest.registered_nodes[v]
 	local tile, _ = def.tiles[1]:gsub("default_", "luscious_")
 	minetest.override_item(v, {
@@ -187,17 +183,21 @@ for _, v in pairs({
 		color = "#7dea1eff",
 		tiles = {tile},
 		place_param2 = 136,
+        visual_scale = 2.3,
 		on_construct = on_construct,
 	})
 end
 
---- I have absolutly no idea what is the purpose but I aplied it to other stuff and it work.
+--- I have no idea what is the purpose but I aplied it to other stuff and it work.
 for i = 1, 5 do
 	minetest.register_alias_force("default:dry_grass_" .. i, "default:grass_" .. i , "default:desert_" .. i , "ethereal:dry_" .. i, "ethereal:prairie_" .. i )
 end
 
--- content ids
+
+
+-- content ids	local v = vector.apply(minp, function(a) return (a - 48) / chunksize end)
 local cn = {
+	["ethereal:bamboo_dirt"] = 1,
 	["ethereal:dry_dirt"] = 1,
 	["ethereal:prairie_dirt"] = 1,
     ["default:sand"] = 1,
@@ -222,44 +222,33 @@ for k, _ in pairs(cn) do
 	cs[minetest.get_content_id(k)] = 1
 end
 
+
+
 minetest.register_on_generated(function(minp, maxp, blockseed)
-	local v = vector.apply(minp, function(a) return (a - 48) / chunksize end)
-	local heatmap = minetest.get_mapgen_object("heatmap")
-	local humiditymap = minetest.get_mapgen_object("humiditymap")
-	local map = ""
-	for i = 1, #heatmap do
-		local h1 = heatmap[i]
-		local h2 = humiditymap[i]
-		h1 = math.floor(math.min(math.max(math.floor(h1), 0), 100) / 6.6)
-		h2 = math.floor(math.min(math.max(math.floor(h2), 0), 100) / 6.6)
-		map = map .. string.char(h1 + (h2 * 16))
-	end
-	local p = string.format("%d", minetest.hash_node_position(v))
-
-	local f = assert(io.open(wp .. "/" .. p, "w"), wp .. "/" .. p)
-	f:write(minetest.compress(map))
-	f:close()
-
+--	local v = vector.apply(minp, function(a) return (a - 48) / chunksize end)
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
 	local data = vm:get_data()
-	local p2data = vm:get_param2_data()
+    local p2data = vm:get_param2_data()
 	for z = minp.z, maxp.z do
 	for y = minp.y, maxp.y do
 		local vi = area:index(minp.x, y, z)
 		for x = minp.x, maxp.x do
 			local vv = (x - minp.x) + ((z - minp.z) * chunksize)
 			if cs[data[vi]] then
-				local mv = cmpy(string.byte(map, vv + 1), y)
-				p2data[vi] = mv
+            local p = vector.new(x, y, z)
+            local mv = chose_color(p)
+            p2data[vi] = mv                
 			end
 			vi = vi + 1
 		end
 	end
 	end
-	vm:set_param2_data(p2data)
-	vm:write_to_map()
+vm:set_param2_data(p2data)
+vm:write_to_map()
 end)
+
+
 
 local function update_p2(pos, size)
 	local minp = {x = pos.x - size[1], y = pos.y, z = pos.z - size[1]}
@@ -270,7 +259,7 @@ local function update_p2(pos, size)
 		local p = vector.new(x, y, z)
 		local node = minetest.get_node(p)
 		if cn[node.name] then
-			on_construct(p)
+        on_construct(p)
 		end
 	end
 	end
